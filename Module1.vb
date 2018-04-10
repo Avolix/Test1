59186cd5655dfd5054afba8a97f6a5bc8f828a7f
Avolix <rex8233@gmail.com>
2018-04-10 13:52:56 -0400

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim testvar As Decimal
+
+        Console.Write("Testvar" & ": ")
+
+        testvar = Console.Read
+
+        Console.ReadKey()
+
+        For test = 1 To 10
+
+        Next
+
+
+
+    End Sub
+
+End Module
